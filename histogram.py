8b03ef3bbdb4d449e82e954817b6228893768c76
Xing_Ye <YOUR_PERSONAL_EMAIL@example.com>
2020-03-02 14:36:33 -0800

    updated histogram

--- histogram.py
+++ histogram.py
@@ -1,86 +1,78 @@
-import sys
+from __future__ import division, print_function
+from random import randint, choice, seed
+seed(1) 
+import re 
 
-word_histogram = {}
-def histogram(lines):
+class Histogram(dict): 
+    def __init__(self, lines=None): 
+        """Initialize this histogram as a new list and count given words."""
+        filename = "words.txt"
+        my_histogram = open(filename,'r')
+        super(Histogram, self).__init__()  # Initialize this as a new list
+        # Add properties to track useful word counts for this histogram
+        self.unique_words_count = 0  #count of unique word 
+        self.words_count = 0  #total count of all words #tokens
+        self.words = [] 
+        if lines != None: 
+            for line in lines:
+                words_from_line = re.sub("[^\w]", " ",  line).split() #turns every word in line to a list of words
+                for word in words_from_line: 
+                    self.count(word)
     
-    for word in lines:
-        words = word.split()
-        #print(words)
-        for w in words:
-            #get method searches for specific key, else it will return 0 as default value
-            word_histogram[w] = word_histogram.get(w,0) + 1
-    return word_histogram
+    def count(self, word, count=1):
+        """Increase frequency count of given word by given count amount."""
+        if self.frequency(word) > 0:
+            self[word] += count # check if the word appears alread if yes add 1 to count.
+        else: 
+            self[word] = count
+            self.unique_words_count += 1 # if an unique word appears, add 1 to unique word count 
+        self.words_count += count
 
-def unique_words(histogram):
+    def frequency(self, word):
+        """Return frequency count of given word, or 0 if word is not found."""
+        if not self.__contains__(word):
+            return 0
+        frequency = self[word]
+        return frequency
+
+    def get_count(self, word):
+        word_count = 0
+        for word in self:
+            word_count = self.get(word, 0) + 1  #get method searches for specific key, else it will return 0 as default value, else adds 1
+            self[word] = word_count
+
+    def __contains__(self, word):
+        """Return boolean indicating if given word is in this histogram."""
+        for word_history in self:
+            if word == word_history:
+                return True
+        return False
+
+def create_histogram(source_text):
+    '''create a Histogram from a string'''
+    return Histogram(source_text)
+
+def get_unique_words(histogram):
+    '''get total count of unique words in the histogram'''
+    return histogram.unique_words
+
+def get_frequency(word, histogram):
+    ''' get the frequency of a word from the histogram'''
+    return histogram.frequency(word)
+
+def word_contains(word, histogram):
+    if histogram.__contains__(word):
+        return True
+    return False
+
+if __name__ == '__main__':
     
-    word_txt = open(filename,'r')
-    count = {}
-    for word in word_txt:
-        if word in count :
-            count[word] += 1
-        else:
-            count[word] = 1
-    len(count) 
-    print(f"number of unique words are {len(count)} ")
-
-
-# def frequency(word，histogram):
-#     frequencies = []
-#     return histogram[word]
-
-def get_index(word,listogram):
-    current_index = 0
-    for item in listogram:
-        if item[0] == word:
-            return current_index
-        else:
-            current_index += 1
-    return -1
-
-
-def listogram(lines):
-    listogram = []
-    for word in lines:
-        word = word.rstrip()
-        index = get_index(word,listogram)
-        if index == -1: #first time we are seeing this 
-            listogram.append([word,1])
-        else: #need to update count
-            listogram[index][1] += 1
-    return listogram
-
-
-def tuplegram(lines):
-    tuplegram = []
-    for word in lines:
-        word = word.rstrip()
-        index = get_index(word,tuplegram)
-        if index == -1: #first time we are seeing this word
-            tuplegram.append((word,1))
-        else:#need to update count
-            newcount = tuplegram[index][1] + 1
-            tuplegram[index] = (word,newcount)
-    return tuplegram
-
-
-
-if __name__ == "__main__":
-
     filename = "words.txt"
-    lines = open(filename,'r')
-
-    histogram(lines)
-
-    unique_words(histogram)
-
-
-
-
-
-
-
-        
-    
-
-
-
+    my_histogram = open(filename,'r')
+    #my_histogram = Histogram("one fish two fish red fish ") #initialize a histogram from a string
+    print(f"Histogram = {my_histogram}")
+    print(f"Unique words count = {my_histogram.unique_words_count}")
+    print(f"Words count = {my_histogram.words_count}")
+    word = "fish"
+    does_contain = f"appears {my_histogram.frequency(word)}x in" if word_contains(word, my_histogram) else "does not appear in"
+    print(f"{word} {does_contain} the following histogram: {my_histogram}")
